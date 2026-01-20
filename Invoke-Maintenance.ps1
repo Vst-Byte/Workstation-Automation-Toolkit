6b948d78b329356d26d0e639824fc2148cc3e793
Matheus Grebos Visentim <09461713940@araucaria.local>
2026-01-20 15:32:35 -0300

    Add some new Options

--- Invoke-Maintenance.ps1
+++ Invoke-Maintenance.ps1
@@ -19,34 +19,49 @@
 function Write-Center {
     param(
         [string]$Texto,
-        [ConsoleColor]$Cor = "Cyan", 
-        [ConsoleColor]$Cor2 = "Green",
+        [ConsoleColor]$Cor = "Yellow",
         [ConsoleColor]$Fundo = "Black" 
     )
 
     $LarguraJanela = $Host.UI.RawUI.WindowSize.Width
     
-    
     $EspacosEsquerda = [math]::Max(0, [int](($LarguraJanela - $Texto.Length) / 2))
     
-
     $TextoCentralizado = (" " * $EspacosEsquerda) + $Texto
 
     Write-Host $TextoCentralizado -ForegroundColor $Cor -BackgroundColor $Fundo
 }
-function Push-ToCenter {
+function Write-Center-Detail {
     param(
-        [int]$TamanhoDoMenu = 12 # Chutei que seu menu ocupa 12 linhas
+        [string]$Label,   
+        [string]$Value,   
+        [ConsoleColor]$CorLabel = "Green", 
+        [ConsoleColor]$CorValue = "White"  
     )
 
-    # 1. Mede a altura da janela
+    $TextoCompleto = "$Label $Value"
+    
+    $LarguraJanela = $Host.UI.RawUI.WindowSize.Width
+    
+    $EspacosEsquerda = [math]::Max(0, [int](($LarguraJanela - $TextoCompleto.Length) / 2))
+    $Margem = " " * $EspacosEsquerda
+
+
+    Write-Host $Margem -NoNewline
+
+    Write-Host "$Label " -ForegroundColor $CorLabel -NoNewline
+    
+    Write-Host $Value -ForegroundColor $CorValue
+}
+function Push-ToCenter {
+    param(
+        [int]$TamanhoDoMenu = 12
+    )
+
     $AlturaJanela = $Host.UI.RawUI.WindowSize.Height
     
-    # 2. Calcula quantas linhas vazias precisa pular no topo
-    # O [math]::Max impede erro se a tela for muito pequena
     $EspacosTopo = [math]::Max(0, [int](($AlturaJanela - $TamanhoDoMenu) / 2))
     
-    # 3. Dá os "Enters" necessários
     for ($i = 0; $i -lt $EspacosTopo; $i++) {
         Write-Host ""
     }
@@ -103,9 +118,8 @@ function AccessDownloadFolder {
         Start-Sleep -Seconds 1
         Move-Item -Path $ArquivoUnico.FullName -Destination $PathFolder   
     }
-    
     Write-Center ""
-    Write-Center "Organization Completed!" -ForegroundColor Green
+    Write-Center "Organization Completed!" -cor Green
     Write-Center ""
 }
 
@@ -113,8 +127,7 @@ function CleanTempFiles {
     Write-Center ""
     Write-Center "Accessing Temp Files..." -ForegroundColor Yellow
     Start-Sleep -Seconds 1
-    # Sua lógica de limpeza virá aqui.
-    # Por enquanto, mantive o que você escreveu:
+
     $FileCleaner = Get-ChildItem -Path $env:TEMP -File -Recurse -ErrorAction SilentlyContinue
     
     if ($FileCleaner) {
@@ -124,6 +137,7 @@ function CleanTempFiles {
     Start-Sleep -Seconds 1
     Write-Center "Cleaning Now..." -ForegroundColor Cyan 
     Start-Sleep -Seconds 1
+
     }else{
     Write-Center "Temp folder is mostly clean or locked." -ForegroundColor Gray
     }
@@ -131,13 +145,110 @@ function CleanTempFiles {
     remove-item -Path $env:temp -Recurse -Force -ErrorAction SilentlyContinue
     Start-Sleep -Seconds 1
     Write-Center ""
-    Write-Center "Cleanup finished! (Locked files were skipped)" -ForegroundColor Green
+    Write-Center "Cleanup finished! (Locked files were skipped)" -Cor Green
+    Write-Center ""
+}
+function Get-SystemInfo {
+    Write-Host ""
+    Write-Center "Colecting System data..." -Cor Yellow
+    Write-Host ""
+
+    $OS = Get-CimInstance Win32_OperatingSystem
+    $CPU = Get-CimInstance Win32_Processor
+    $Disco = Get-Volume -DriveLetter C
+    $BIOS = Get-CimInstance Win32_BIOS 
+    $GPU = Get-CimInstance Win32_VideoController
+    
+    try {
+        $Rede = Get-NetIPConfiguration | Where-Object { $_.IPv4DefaultGateway -ne $null } | Select-Object -First 1
+        $IPAddress = $Rede.IPv4Address.IPAddress
+    } catch {
+        $IPAddress = "Not Detected"
+    }
+
+    $MemoriaTotal = [math]::Round($OS.TotalVisibleMemorySize / 1MB, 2)
+    $MemoriaLivre = [math]::Round($OS.FreePhysicalMemory / 1MB, 2)
+    $DiscoLivre   = [math]::Round($Disco.SizeRemaining / 1GB, 2)
+    $DiscoTotal = [math]::Round($Disco.Size / 1GB, 2)
+
+    Write-Center-Detail -Label "Hostname:" -Value "$($OS.CSName)" 
+    Write-Center-Detail ""
+    Start-Sleep -Seconds 1
+    Write-Center-Detail -Label "Serial Number:" -Value "$($BIOS.SerialNumber)" 
+    Write-Center-Detail ""
+    Start-Sleep -Seconds 1
+    Write-Center-Detail -Label "IP Address:" -Value "$IPAddress" 
+    Write-Center-Detail ""
+    Start-Sleep -Seconds 1
+    Write-Center-Detail -Label "System:" -Value "$($OS.Caption) ($($OS.OSArchitecture))" 
+    Write-Center-Detail ""
+    Start-Sleep -Seconds 1
+    Write-Center-Detail -Label "CPU:" -Value "$($CPU.Name)" 
+    Write-Center-Detail ""
+    Start-Sleep -Seconds 1
+    Write-Center-Detail -Label "RAM:" -Value "${MemoriaLivre} GB Free from ${MemoriaTotal} GB" 
+    Write-Center-Detail ""
+    Start-Sleep -Seconds 1
+    Write-Center-Detail -Label "GPU:" -Value "$($GPU.Name)" 
+    Write-Center-Detail ""
+    Start-Sleep -Seconds 1
+    Write-Center-Detail -Label "Disk (C:):" -Value "${DiscoLivre} GB Free from: ${DiscoTotal}" 
+    Write-Center-Detail ""
+    Start-Sleep -Seconds 1
+    $Uptime = (Get-Date) - $OS.LastBootUpTime
+    Write-Center -Label "Time On:" -Value "$($Uptime.Days) days, $($Uptime.Hours) hours, $($Uptime.Minutes) min" 
+    
+    Write-Center ""
+}
+function Repair-Network {
+    Write-Center ""
+    Write-Center "EXECUTING NETWORK REPAIR..." -Cor Yellow
+    Write-Center ""
+    Start-Sleep -Seconds 1
+    Write-Center-Detail -label "1." -Value "Clearing DNS Cache..."
+    Write-Center ""
+    Start-Sleep -Seconds 1
+    Clear-DnsClientCache
+    Write-Center "[OK] Cache Cleared." -Cor Green
+    Write-Center ""
+    Start-Sleep -Seconds 1
+    
+    Write-Center-Detail -label "2." -Value  "Renewing IP Address..." 
+    Write-Center ""
+    Write-Center "Disclaimer: This may cause a brief disconnection)" -Cor Red
+    Write-Center ""
+    ipconfig /renew | Out-Null
+    Write-Center "[OK] IP Renewed." -Cor Green
+
+    Write-Host ""
+}
+function Get-ErrorLogs {
+    Write-Center ""
+    Write-Center "SCANNING SYSTEM ERROR LOGS..." -Cor Yellow
     Write-Center ""
 
+    $Errors = Get-EventLog -LogName System -EntryType Error -Newest 5 -ErrorAction SilentlyContinue
+
+    if ($Errors) {
+        foreach ($E in $Errors) {
+            Write-Center "Time: $($E.TimeGenerated)" -Cor Cyan
+            Write-Center ""
+            Write-Center "Source: $($E.Source)" -Cor White
+            Write-Center ""
+            Write-Center "Message: $($E.Message)" -Cor Gray
+            Write-Center ""
+            Write-Center "---------------------------------" -Cor DarkGray
+            Write-Center ""
+        }
+    } else {
+        Write-Center "No critical errors found recently. System is healthy!" -Cor Green
+    }
+
+    Write-Host ""
 }
 
 # ==========================================
-# EXECUÇÃO PRINCIPAL (MENU)
+#         EXECUÇÃO PRINCIPAL (MENU)
 # ==========================================
 
 $Sair = $False 
@@ -146,36 +257,58 @@ do {
     Clear-Host
 
     Push-ToCenter -TamanhoDoMenu 12
-
-    Write-Center "=========================================" -ForegroundColor Cyan
-    Write-Center "       File Organizer/File Cleaner       " -ForegroundColor Cyan
-    Write-Center "=========================================" -ForegroundColor Cyan
+    
+    Write-Center "=========================================" 
+    Write-Center "           Workstation-Automated         " 
+    Write-Center "=========================================" 
     Write-Center ""
     Write-Center "  1. Organize Files (Downloads)            "
+    Write-Center ""
     Write-Center "  2. Clean Files (%Temp%)                  "
-    Write-Center "  3. Exit                                  "
+    Write-Center ""
+    Write-Center "  3. Collect System Data                   "
+    Write-Center ""
+    Write-Center "  4. Repair Network Connection             "
+    Write-Center ""
+    Write-Center "  5. Collect ErrorLogs                     "
+    Write-Center ""
+    Write-Center "  6. Exit                                  "
     Write-Center "-----------------------------------------"
     Write-Center ""
     
-    $Opcao = Read-Center " Choose an Option "   
-
+    $Opcao = Read-Center " Choose an Option " -Cor White
+    
     switch ($Opcao) {
         "1" { 
             AccessDownloadFolder
-            Read-Center "Press ENTER to return to the menu..." 
+            Read-Center "Press ENTER to return to the menu..." -Cor Gray
         }
         "2" { 
             CleanTempFiles
-            Read-Center "Press ENTER to return to the menu..." 
+            Read-Center "Press ENTER to return to the menu..." -Cor Gray
         }
-        "3" { 
+        "3"{
+            Get-SystemInfo
+            Read-Center "Press ENTER to return to the menu..." -Cor Gray
+        }
+        "4"{
+            Repair-Network
+            Read-Center "Press ENTER to return to the menu..." -Cor Gray
+        }
+        "5"{
+            Get-ErrorLogs
+            Read-Center "Press ENTER to return to the menu..." -Cor Gray
+        }
+        "6" { 
             Write-Center ""
-            Write-Center "Closing..." -ForegroundColor Red
+            Write-Center "Closing..." -Cor Red
             Start-Sleep -Seconds 1
             $Sair = $True 
-        }
+        } 
+      
         Default { 
-            Write-Center "Invalid Option!" -ForegroundColor Red
+            Write-Center ""
+            Write-Center "Invalid Option!" -Cor Red
             Start-Sleep -Seconds 1
         }
     }
